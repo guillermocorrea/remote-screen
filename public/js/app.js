0883364dd76ae2f514c7f9c5d8498d03a7b8ee97
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2014-08-26 23:37:17 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by guillermo on 26/08/2014.
+ */
+var key = "secret";
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+        Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+        Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+    );
+}
+var socket = io.connect('http://localhost:3000');
+var y = 0;
+
+socket.emit('setKey', key);
+socket.on('scrollTo', function (data) {
+    if (data.y == 'max') {
+        y = getDocHeight();
+    } else {
+        if (data.action == 'ffd') {
+            y += parseInt(data.y);
+        } else if (data.action == 'go') {
+            y = parseInt(data.y);
+        } else {
+            y -= parseInt(data.y);
+        }
+    }
+    window.scrollTo(0, y);
+
+});
+document.write('<img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=http://localhost:3000/panel/' + key + '&choe=UTF-8" alt=""/>');
---
+++ server.js
@@ -0,0 +1,38 @@
+/**
+ * Created by guillermo on 26/08/2014.
+ */
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var express = require('express');
+
+server.listen(3000);
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/panel/:key', function (req, res) {
+    var key = req.params.key;
+    console.log(key);
+    res.sendFile(__dirname + '/public/mobile.html');
+});
+
+app.get('/action/:key/:y/:action', function (req, res) {
+    var key = req.params.key;
+    var y = req.params.y;
+    var action = req.params.action;
+    sockets[key].emit('scrollTo', {y:y, action:action});
+    res.send('OK');
+});
+
+var sockets = {};
+io.on('connection', function (socket) {
+    socket.on('setKey', function (key) {
+        sockets[key] = socket;
+    });
+});
+
+console.log('listening on port 3000');
